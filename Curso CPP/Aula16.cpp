11ea566ee950647d2061c6312259a25d483e267e
Daniel Brasil <danieldfb@gmail.com>
2022-05-28 23:55:45 -0300

    att 2805

---
+++ Curso CPP/Aula16.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int n;
+
+    n = 10;
+
+    do {
+        cout << n << "\n";
+        n++;
+        
+    } while (n<9);
+
+    cout << "\nRotina finalizada.\n\n";
+    
+    
+    return 0;
+
+}
